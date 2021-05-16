3ca7831dec78cba3f9d6484e321c32af3048c23e
josh jacques <jp.jacques@yahoo.com>
2021-05-16 14:47:25 -0500

    test

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { NavBar } from './Styled.js';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
   return (
     <React.Fragment>
       <div>
         <h1 className='text-center mt-5'>Online Based Cloud Pizza Delivery Service</h1>
+          <NavBar className='list-unstyled lead d-flex justify-content-around'>
+            <NavLink id='order-pizza' to='/' className=''><b>Home</b></NavLink>
+            <NavLink to='/menu' className=''><b>Toppings</b></NavLink>
+            <NavLink to='/pizza' className=''><b>Build Your Pizza</b></NavLink>
+          </NavBar>
       </div>
     </React.Fragment>
   );
